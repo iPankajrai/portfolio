3fd06fa3a1c083b7b28c7c1e836745a8532531ef
Pankaj Rai <pankaj200497@gmail.com>
2021-02-26 16:25:52 +0530

    Javascript for portfolio section added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /* -------------------- About Section tabs -------------------- */
 (() =>{
-		console.log('Hello World!');
+		// console.log('Hello World!');
 		const aboutSection = document.querySelector(".about-section"),
 			tabsContainer = document.querySelector(".about-tabs");
 			tabsContainer.addEventListener("click", (event) => {
@@ -9,7 +9,7 @@
 					/* console.log("event.target contains 'tab-item' class and not contains 'active' class ");
 					console.log(event.target); */
 					const target = event.target.getAttribute("data-target");
-					console.log(target);
+					// console.log(target);
 					// deactivate existing active 'tab-item'
 					tabsContainer.querySelector(".active").classList.remove("outer-shadow", "active");
 					// activate new 'tab-item'
@@ -17,7 +17,7 @@
 					// deactivate existing active 'tab-content'
 					aboutSection.querySelector(".tab-content.active").classList.remove("active");
 					// activate the 'tab-content'
-					console.log(aboutSection.querySelector(target));
+					// console.log(aboutSection.querySelector(target));
 					aboutSection.querySelector(target).classList.add("active"); 
 				}
 			})
@@ -39,7 +39,33 @@
 
 		/* filter portfolio items */
 		filterContainer.addEventListener("click", (event) => {
-			console.log(event.target);
+			// console.log(event.target);
+			if(event.target.classList.contains("filter-item") && !event.target.classList.contains("active")){
+				// console.log("true");
+				// deactivate existing active 'filter-item'
+				filterContainer.querySelector(".active").classList.remove("outer-shadow", "active");
+				// activate new 'filter-item'
+				event.target.classList.add("active", "outer-shadow");
+				const target = event.target.getAttribute("data-target");
+				// console.log(target);
+				portfolioItems.forEach((item) => {
+					// console.log(item);
+					// console.log(item.getAttribute("data-category"));
+					if(target === item.getAttribute("data-category") || target === "all"){
+						item.classList.remove("hide");
+						item.classList.add("show");
+					}
+					else{
+						item.classList.remove("show");
+						item.classList.add("hide");
+					}
+				})
+
+			}
+			else{
+				// console.log("false");
+			}
+			// console.log(event.target);
 		}) 
 
 
